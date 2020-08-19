ee34bcbd2e04eaee6d619452188e6c4f12574c70
Trevor James <trevorwillemjames@gmail.com>
2020-08-19 14:57:50 -0700

    most work from today

--- twitoff/app.py
+++ twitoff/app.py
@@ -4,6 +4,7 @@ Main app/routing file for TwitOff
 from flask import Flask, render_template, request
 from twitoff.models import DB, User
 from twitoff.twitter import insert_example_user, add_or_update_user
+from twitoff.predict import predict_user
 
 
 def create_app():
@@ -12,7 +13,6 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
-
 
     @app.route('/')
     def root():
@@ -33,17 +33,19 @@ def create_app():
         return render_template('user.html', title=name, tweets=tweets, message=message)
 
     @app.route('/compare', methods=['POST'])
-    def compare():
+    def compare(message=''):
         user1, user2 = sorted([request.values['user1'],
                                request.values['user2']])
         if user1 == user2:
-            message = 'Must compare 2 different twitter users'
+            message = 'Cannot compare a user to themselves!'
         else:
-            prediction = predict_user(user1, user1, request.values['tweet_text'])
-            message = '"{}" is more likely to be said by {} than {}'.format(request.values['tweet_text'],
-                                                                            user1 if prediction[0] else user2,
-                                                                            user2 if prediction[0] else user1)
-        return render_template('prediciton.html', title='Prediction', message=message)
+            prediction = predict_user(user1, user2,
+                                      request.values['tweet_text'])
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], user1 if prediction else user2,
+                user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction',
+                               message=message)
 
     @app.route('/update')
     def update():
@@ -60,7 +62,7 @@ def create_app():
     @app.route('/trevor')
     def trevor():
         hi = 'Hello Human, I am Trevor THE creator of TwitOff, well at least this version of twitoff...' \
-             'designed by Lambda School, with a walkthrough by Aaron Gallant, and with lots of help from ' \
+             'designed by Lambda School, with a although by Aaron Gallant, and with lots of help from ' \
              'my TL Broken, and all my fellow students...'
         return hi
 
